94a445881858ede9ad8bc744d5df60f5974b7ab0
1qOnPass <104042831+1qOnPass@users.noreply.github.com>
2025-05-02 16:14:20 +0900

    Update gt-test.js

--- css/gt-test.js
+++ css/gt-test.js
@@ -11,7 +11,7 @@ function openAppOrRedirect() {
         appUrl = "intent://passapp/#Intent;scheme=1qonpass;package=com.hanati.pass;end;";
         // storeUrl이 미리 정의되어 있지 않으면 기본값 사용
         if (typeof window.storeUrl === "undefined") {
-            window.storeUrl = "https://play.google.com/store/apps/details?id=com.hanati.pass";
+            window.storeUrl = "market://details?id=com.hanati.pass";
         }
     } else if (isIOS) {
         appUrl = "oneqpass://";
